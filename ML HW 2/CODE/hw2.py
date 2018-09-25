a128a88494b3f2e328171fd9fe4bc2487b9ee416
James Brissette <brissette@sci.utah.edu>
2018-09-25 17:54:46 -0600

    4of5 algorithms implemented, finish 5 and begin CV

--- ML HW 2/CODE/hw2.py
+++ ML HW 2/CODE/hw2.py
@@ -13,6 +13,32 @@ INIT_PARAM = np.random.randint(-100,100) / 10000.
 
 
 def simple_perceptron(data, rate, w, b):
+    mistakes = 0
+    for example in data:
+        yi = 0;
+        pred = 0
+        for key in list(example.keys()):
+            if key == 'label':
+                yi = example[key];
+            else:
+                pred += w[key-1]*example[key]
+        pred += b
+        
+        if (yi * pred) <= 0:
+            mistakes += 1
+            for key in list(example.keys()):
+                if key == 'label':
+                    continue
+                else:
+                    w[key-1] += rate*yi*example[key]
+            b += rate*yi
+            
+    print('Mistakes = ' + str(mistakes))
+    return w, b
+
+def decaying_perceptron(data, rate, w, b, t):
+    decayed_r = rate / (1+t)
+    mistakes = 0
     for example in data:
         yi = 0;
         pred = .01;
@@ -23,31 +49,76 @@ def simple_perceptron(data, rate, w, b):
                 pred += w[key-1]*example[key]
         pred += b
         
-        if (yi * pred) < 0:
-#            print('Incorrect prediction at epoch=' + str(epoch))
-            #update weights and bias
+        if (yi * pred) <= 0:
+            mistakes += 1
             for key in list(example.keys()):
                 if key == 'label':
                     continue
                 else:
-                    w[key-1] += rate*yi*example[key]
+                    w[key-1] += decayed_r*yi*example[key]
             b += rate*yi
-        else:
-            continue
-#            print('Correct prediction at epoch=' + str(epoch))
-    return w, b
+            decayed_r = rate / (1+t)
+#            print('At t=' + str(t) + ' rate = ' + str(rate) + '/' + str(1+t) + '=' + str(decayed_r))
+        
+        t += 1
+    print('mistakes = ' + str(mistakes))
+    return w, b, t
 
-def decaying_perceptron():
-    
-    return
+def margin_perceptron(data, rate, w, b, t, mu):
+    decayed_r = rate / (1+t)
+    mistakes = 0
+    for example in data:
+        yi = 0;
+        pred = 0;
+        for key in list(example.keys()):
+            if key == 'label':
+                yi = example[key];
+            else:
+                pred += w[key-1]*example[key]
+        pred += b
+        
+        if (yi * pred) <= mu:
+            mistakes += 1
+            for key in list(example.keys()):
+                if key == 'label':
+                    continue
+                else:
+                    w[key-1] += decayed_r*yi*example[key]
+            b += rate*yi
+            decayed_r = rate / (1+t)
+            print('At t=' + str(t) + ' rate = ' + str(rate) + '/' + str(1+t) + '=' + str(decayed_r))
+        
+        t += 1
+    print('mistakes = ' + str(mistakes))
+    return w, b, t
 
-def margin_perceptron():
-    
-    return
-
-def average_perceptron():
-    
-    return
+def average_perceptron(data, rate, w, b, avg_w, avg_b):
+    for example in data:
+        yi = 0;
+        pred = 0;
+        for key in list(example.keys()):
+            if key == 'label':
+                yi = example[key];
+            else:
+                pred += w[key-1]*example[key]
+        pred += b
+        
+#       update w only on error, update a regardless
+        for key in list(example.keys()):
+            if key == 'label':
+                continue
+            else:
+                if (yi * pred) <= 0:
+                    print('updating w')
+                    w[key-1] += rate*yi*example[key]
+                print('updating a')
+                avg_w[key-1] += rate*yi*example[key]
+                
+        if (yi * pred) <= 0:
+            b += rate*yi
+        avg_b += rate*yi
+        print('')
+    return w, b, avg_w, avg_b
 
 def aggressive_perceptron():
     
@@ -98,6 +169,23 @@ def loadData(fpath):
 
 data = loadData('diabetes.train')
 
-w = np.ones(DATA_DIM)*np.random.randint(-100,100) / 10000.
-b = np.random.randint(-100,100) / 10000.
-simple_perceptron(data, .01, w, b)
+#simple perceptron
+rate = .01
+w = np.ones(DATA_DIM)*np.random.randint(-10000,10000) / 1000000.
+print(str(w))
+b = np.random.randint(-10000,10000) / 1000000.
+
+# t for decaying/margin
+t = 0
+
+#mu for margin
+mu = 0
+
+#a/b for avgerage
+avg_w = np.zeros(DATA_DIM)
+avg_b = 0
+
+#simple_perceptron(data, rate, w, b)
+#decaying_perceptron(data, rate, w, b, t)
+#margin_perceptron(data, rate, w, b, t, mu)
+x,y,z,aa = average_perceptron(data, rate, w, b, avg_w, avg_b)
